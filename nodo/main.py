b7b2e78d26d5300d0c74721c6e24784c5edb9aba
dfr2098 <edsiodfr@gmail.com>
2024-02-09 16:47:14 -0600

    Actualizaciones de código: archivo requirements.txt y docker-compose.yml, y agregado de manejo de excepciones en main.py

--- nodo/main.py
+++ nodo/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
+@app.on_event("startup")
+async def startup_event():
+    client = AsyncIOMotorClient("mongodb://dfr209811:nostromo987Q_Q@172.45.0.4:27017/tennus_data_analitica")
+    db = client.tennus_data_analitica
+    if not await db.list_collection_names():
+        raise HTTPException(status_code=500, detail="No se pudo conectar a MongoDB")
 
-@app.get("/user")
-def read_root():
-    return {"Mensaje": "Hola Mundo"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+    
+    
+    
+    
