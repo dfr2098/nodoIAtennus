84febae0708f62c6ffae4c49a6a6a73149be2644
dfr2098 <edsiodfr@gmail.com>
2024-02-15 23:31:09 -0600

    Agrega archivos .gitignore y docker-compose.yml, modifica main.py y agrega supervisord.log y supervisord.pid

--- nodo/main.py
+++ nodo/main.py
@@ -1,18 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 
+client = AsyncIOMotorClient('mongodb://localhost:27017')
+db = client.mydatabase
+
 app = FastAPI()
-@app.on_event("startup")
-async def startup_event():
-    client = AsyncIOMotorClient("mongodb://dfr209811:nostromo987Q_Q@172.45.0.4:27017/tennus_data_analitica")
-    db = client.tennus_data_analitica
-    if not await db.list_collection_names():
-        raise HTTPException(status_code=500, detail="No se pudo conectar a MongoDB")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-    
-    
-    
-    
