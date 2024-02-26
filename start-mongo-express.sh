9e291bf5203251fef829d71c69fba2bd90bc3e95
dfr2098 <edsiodfr@gmail.com>
2024-02-25 19:26:45 -0600

    Agregado script para iniciar mongo-express y actualizado archivo hola.ipynb

---
+++ start-mongo-express.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Espera 30 segundos
+echo "Esperando 30 segundos para que MongoDB esté listo..."
+sleep 30
+
+# Imprime un mensaje y luego deja que el entrypoint predeterminado tome el control
+echo "Mongo-express debería estar iniciándose ahora..."
