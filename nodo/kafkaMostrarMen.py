3934099a81b0a893dacd88ed7695f0824fb1bffb
Sammoga123 <l191080006@iztapalapa.tecnm.mx>
2024-04-06 19:02:31 -0600

    agregar kafkaMostrarMen

---
+++ nodo/kafkaMostrarMen.py
@@ -0,0 +1,48 @@
+import asyncio
+import json
+from fastapi import FastAPI
+from kafka import KafkaConsumer, TopicPartition
+
+app = FastAPI()
+
+# Configura el consumidor de Kafka
+consumer = KafkaConsumer(
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',  # Para empezar desde el principio
+    enable_auto_commit=False,  # Desactiva el auto commit para tener control manual
+    value_deserializer=lambda x: x.decode('utf-8')  # Decodifica los mensajes como cadenas
+)
+
+async def consumir_mensajes_kafka():
+    particion_topic = TopicPartition('json_topic', 0)  # Partición 0 de 'json_topic'
+    consumer.assign([particion_topic])
+    consumer.seek_to_beginning(particion_topic)  # Se posiciona al principio del topic
+
+    mensajes = []
+    while True:
+        batch = consumer.poll(timeout_ms=100)  # Espera mensajes durante 100 ms
+        if batch:
+            for _, records in batch.items():
+                for record in records:
+                    try:
+                        json_obj = json.loads(record.value)
+                        mensajes.append(json_obj)
+                    except ValueError:
+                        # Agrega un diccionario indicando que el mensaje no es válido
+                        mensajes.append({"mensaje": record.value, "error": "Mensaje no válido"})
+        else:
+            break
+    return mensajes
+
+
+"""
+#Obtener mensajes pasados de Kafka en API
+@app.get("/Mensajes de kafka")
+async def obtener_mensajes_pasados_kafka():
+    mensajes = await asyncio.gather(consumir_mensajes_kafka())
+    return mensajes[0]
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
+"""
